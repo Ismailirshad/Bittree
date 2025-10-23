1f289ab3ed0d5e2758644de37e35e6fc12a37166
Ismailirshad <irshadsha164@gmail.com>
2025-10-23 22:11:08 +0530

    bugs fixed

--- app/generate/page.js
+++ app/generate/page.js
@@ -1,7 +1,8 @@
 export const dynamic = 'force-dynamic'
+export const revalidate = 0
 "use client"
 
-import React, { useState } from 'react'
+import React, { useState , Suspense} from 'react'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useSearchParams } from 'next/navigation';
