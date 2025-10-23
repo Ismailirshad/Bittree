06cacf0640d752d388027d6ec5a3f68ed5667f3c
Ismailirshad <irshadsha164@gmail.com>
2025-10-23 21:09:15 +0530

    bugs fixed

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 "use client"
-import Image from "next/image";
+// import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
@@ -12,14 +12,14 @@ export default function Home() {
   }
   return (
     <>
-      <section className="bg-green-800 h-[100vh] grid grid-cols-2" >
+      <section className="bg-green-800 h-[100vh] grid grid-cols-2 " >
         <div className="flex  flex-col items-center justify-center">
           <div className="w-[550px] h-full flex flex-col items-center justify-center text-start gap-5">
             <h1 className="text-6xl text-yellow-400 font-bold">Everything you are. In one, simple link in bio</h1>
             <p className="text-xl text-white">Join 70M+ people using Linktree for their link in bio. One link to help you share everything you create,
               curate and sell from your Instagram, TikTok, Twitter, YouTube and other social media profiles.</p>
             <div className="flex gap-4">
-              <input type="text" value={text} onChange={(e) => { settext(e.target.value) }} className="bg-amber-50 text-black p-2 rounded-xl" placeholder="Enter your Handle" />
+              <input type="text" value={text} onChange={(e) => { settext(e.target.value) }} className="bg-amber-50 text-black p-2 rounded-xl" placeholder="Enter your Handle"  suppressHydrationWarning />
               <button onClick={() => createTree()} className="bg-pink-300 text-black px-3 font-semibold rounded-2xl">Claim Your Linktree</button>
             </div>
           </div>
