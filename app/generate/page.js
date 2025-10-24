d82a1bf59be8085e2c88033e34ecfe3784a7e86d
Ismailirshad <irshadsha164@gmail.com>
2025-10-24 09:51:38 +0530

    bugs fixed

--- app/generate/page.js
+++ app/generate/page.js
@@ -1,16 +1,7 @@
-import { Suspense } from 'react'
+'use client'
+
 import Generate from '../components/GenerateContent'
 
-export const dynamic = 'force-dynamic'
-export const revalidate = 0
-export const fetchCache = 'force-no-store'
-
 export default function GeneratePage() {
-  return (
-    <Suspense fallback={<div className="bg-[#2c2c2c] h-screen flex items-center justify-center text-white">
-      Loading...
-    </div>}>
-      <Generate />
-    </Suspense>
-  )
+  return <Generate />
 }
