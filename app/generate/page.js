ada5ceb9890e6c38a640fd7007bb4775beab74f4
Ismailirshad <irshadsha164@gmail.com>
2025-10-24 18:47:24 +0530

    searchParams bugs fixed

--- app/generate/page.js
+++ app/generate/page.js
@@ -1,12 +1,5 @@
-'use client'
-import { useSearchParams } from 'next/navigation'
-import Generate from '../components/GenerateContent'
+import GenerateContent from '../components/GenerateContent'
 
 export default function GeneratePage() {
-  const searchParams = useSearchParams()
-  const handle = searchParams.get('handle')
-  
-  return <Generate initialHandle={handle} />
+  return <GenerateContent />
 }
-
-export const dynamic = 'force-dynamic'
